90052522f82cd03935f30473a8cc6a30ceff09f0
JuniorKHAIdeveloper <a.g.ivanov@student.csn.khai.edu>
2023-12-20 18:15:38 +0200

    Добавьте файлы проекта.

---
+++ lr5_2/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace lr5_2
+{
+    public class Logger
+    {
+        private readonly string filePath;
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(5, 5); 
+
+        public Logger(string filePath)
+        {
+            this.filePath = filePath;
+            ClearLogFile();
+        }
+
+        private void ClearLogFile()
+        {
+            File.WriteAllText(filePath, string.Empty);
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                semaphore.Wait(); 
+                File.AppendAllText(filePath, message + Environment.NewLine);
+            }
+            finally
+            {
+                semaphore.Release(); 
+            }
+        }
+    }
+}
---
+++ lr5_2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+
+namespace lr5_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Logger logger;
+        private List<Thread> threads;
+        public MainWindow()
+        {
+            InitializeComponent();
+            logger = new Logger("file.txt");
+            threads = new List<Thread>();
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            int threadCount;
+            if (int.TryParse(threadCountTextBox.Text, out threadCount) && threadCount >= 5 && threadCount <= 20)
+            {
+                StartThreads(threadCount);
+            }
+            else
+            {
+                MessageBox.Show("Введите число от 5 до 20.");
+            }
+        }
+        private void StartThreads(int threadCount)
+        {
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(ThreadMethod);
+                threads.Add(thread);
+                thread.Start(i + 1);
+            }
+        }
+        private void ThreadMethod(object threadNumberObj)
+        {
+            int threadNumber = (int)threadNumberObj;
+            Random random = new Random();
+            int randomDelay = random.Next(1000);
+            Thread.Sleep(randomDelay);
+            DateTime currentTime = DateTime.Now;
+            string logMessage = $"Thread {threadNumber}: {currentTime:HH:mm:ss.fff} - Delay: {randomDelay} ms";
+            System.Threading.Monitor.Enter(logger);
+            logger.Log(logMessage);
+            System.Threading.Monitor.Exit(logger);
+            Dispatcher.InvokeAsync(() =>
+            {
+                threadStatusList.Items.Add(logMessage);
+            });
+        }
+    }
+}
